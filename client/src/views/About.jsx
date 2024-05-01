76d42ac243dc131f405f76d6cf7894792b7f6008
antorrg <114789901+antorrg@users.noreply.github.com>
2024-05-01 00:58:18 -0300

    update styles about

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -7,7 +7,6 @@ const About = () => {
   return (
     <div className={style.body}>
       <div className={style.about}>
-      <div classname={style.textContainer}>
       <div className={style.head}>
       <h1 >Acerca de mi:</h1>
       <a style={{ color:'#0ccac4'}}href='/'><h4>Volver</h4></a>
@@ -16,7 +15,10 @@ const About = () => {
       <img className= {style.image}src= 'https://res.cloudinary.com/dt1lpgumr/image/upload/v1714247374/foto-perfil_d8p6pl.webp' alt = 'not Found'/>
       <p style={{textAlign:'left', color:'white', margin:'2rem', marginTop: 'none'}}>El proyecto de Rick and Morty formó parte del bootcamp ya que fue el proyecto integrador, y a partir del M2 hasta finalizar el bootcamp fue el proyecto en el cual ibamos plasmando lo aprendido. No estando yo conforme con la manera en que lo habia llevado a cabo, y al termino de varios intentos, pude plasmar algo con lo que me encuentro medianamente de acuerdo. Pude implementar aquí autenticacion con jsonwebtoken, paginado, carousel, busqueda por nombre, persistencia de datos por medio de sequelize y base de datos postgres sql, y suspensión de pantalla o suspensión de inactividad ("Idle Timeout" o "Screen Timeout), ya que al tener un carousel continuo en la landing debía economizar recursos. Quizás dentro de un tiempo termine cambiando otra vez la pagina o no, pero a la fecha resume lo aprendido por mi.</p>
       <br></br>
-      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       </div>
       </div>
   )
